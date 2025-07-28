701cf68ce2594d393ed832ef97c62fc71889a968
Tom Watt <tom@buildkite.com>
2025-07-28 14:13:32 +0100

    feat: pass env vars to container instead of using flags to remove for job logs

--- lib/plugin.bash
+++ lib/plugin.bash
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Use WIZ_CLIENT_ID and WIZ_CLIENT_SECRET environment variables to authenticate to Wiz and get auth file
+# $1 - Wiz CLI Container Image 
+# $2 - Directory to store auth file
 setupWiz() {
     local wiz_container_image="${1}"
     local wiz_dir="${2}"
@@ -9,11 +12,13 @@ setupWiz() {
     docker run \
         --rm -it \
         --mount type=bind,src="${wiz_dir}",dst=/cli \
+        -e WIZ_CLIENT_ID \
+        -e WIZ_CLIENT_SECRET \
         "${wiz_container_image}" \
-        auth --id="${WIZ_API_ID}" --secret="${!api_secret_var}"
+        auth
     # check that wiz-auth work expected, and a file in WIZ_DIR is created
     if [ -z "$(ls -A "${wiz_dir}")" ]; then
-        echo "Wiz authentication failed, please confirm that credentials are set for WIZ_API_ID and WIZ_API_SECRET"
+        echo "Wiz authentication failed, please confirm that credentials are set for WIZ_CLIENT_ID and WIZ_CLIENT_SECRET"
         exit 1
     else
         echo "Authenticated successfully"
